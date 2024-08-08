25f4d4ce496b97ab7599c9a8f5e44f77929ccd3f
Nimanoia1 <nimakhosravi6455@gmail.com>
2024-08-08 18:36:15 +0330

    added tiles and card draw logic

---
+++ Playspace.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var CardsDatabase = preload("res://Assets/Cards/CardsDatabase.gd").new()
+@onready var tile_base = preload("res://Tiles/tile_base.gd").new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
